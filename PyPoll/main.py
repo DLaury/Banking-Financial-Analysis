83b533fd749d0e011cb4c225c8a683d902e93c81
Dan Laury <DLaury09@gmail.com>
2018-10-13 01:15:02 -0500

    Figuring Out Dictionaries

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,6 +4,7 @@ import os
 
 #initializing variables
 numVotes = 0
+candidates = {}
 
 #opening file election_data.csv and reading it
 electionData = os.path.join('PyPoll', 'Resources', 'election_data.csv')
@@ -18,12 +19,19 @@ with open(electionData, 'r', newline = "") as csvfile:
         
         #adding to numVotes and totChange for total values
         numVotes += 1
+        
+        if row[2] not in candidates:
+            candidates[row[2]] = 0
+
+        candidates[row[2]] += 1        
 
 #formatting output
 output = (f"\nElection Results\n"
         f"---------------------------\n"
         f"Total Votes: {numVotes}\n"
-        f"---------------------------\n")
+        f"---------------------------\n"
+        f"")
 
 #printing output
 print(output)
+print(candidates)
